eac12d14714c2cf9486f0e83330cbb2971d07cf8
Mingyuan Xia <ken.mingyuan@gmail.com>
2017-02-08 17:16:45 +0800

    add Scope and downgrade guava to 20 for Java7 on Travis

---
+++ src/main/java/patdroid/core/Scope.java
@@ -0,0 +1,26 @@
+package patdroid.core;
+
+import java.util.Collection;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * A scope is a container of classes. A scope can be used to represent different entities, which
+ * entirely depends on the upper layer program analysis task.
+ * Just to like a few things that a scope can stand for:
+ * An APK file (as a whole), a Dex file (consider multi-dex), android framework classes (framework.jar)
+ * Java core library classes (java.*), app code (e.g. with a specific package name).
+ *
+ * Overall it is suggested multiple scopes stay disjoint.
+ */
+public class Scope {
+    private final ConcurrentHashMap<String, ClassInfo> classes = new ConcurrentHashMap<String, ClassInfo>();
+    public ClassInfo findClass(String fullName) { return classes.get(fullName); }
+    public ClassInfo createClass(String fullName) { return new ClassInfo(fullName); }
+    public boolean hasClass(ClassInfo ci) { return classes.containsValue(ci); }
+    public Collection<ClassInfo> getAllClasses() { return classes.values(); }
+    public Collection<String> getAllClassNames() { return classes.keySet(); }
+    public ClassInfo findOrCreateClass(String fullName) {
+        ClassInfo u = findClass(fullName);
+        return (u == null ? createClass(fullName) : u);
+    }
+}
